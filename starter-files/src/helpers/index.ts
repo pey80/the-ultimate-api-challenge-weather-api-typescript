39578095b59ec6bfd65fc4edee6c30276b5c35d2
Peyton Menzies <peyton.menzies@thoughtworks.com>
2023-02-16 16:04:33 +1000

    Update times to use 12 hour time

--- starter-files/src/helpers/index.ts
+++ starter-files/src/helpers/index.ts
@@ -29,15 +29,19 @@ export const getVisibilityValue = (number: number): string => {
   return 'Very clear day'
 }
 
-export const getSunTime = (timestamp: number): string => {
-  const date = new Date(timestamp * 1000)
-  let hours = date.getHours().toString()
-  let minutes = date.getMinutes().toString()
+export const getSunTime = (timestamp: number, timezone: number): string => {
+  const date = new Date((timestamp + timezone) * 1000)
 
-  if (hours.length <= 1) hours = `0${hours}`
-  if (minutes.length <= 1) minutes = `0${minutes}`
+  console.log(new Date((timestamp + timezone) * 1000).toUTCString())
+  let hours = date.getUTCHours()
+  const minutes = date.getUTCMinutes()
 
-  return `${hours}:${minutes}`
+  if (hours > 12) {
+    hours = hours - 12
+    return `${hours}:${minutes} pm`
+  }
+
+  return `${hours}:${minutes} am`
 }
 
 export const getPop = (value: number): string => {
